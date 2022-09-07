9612602e6625e8ea36880ab90d4444775e929b83
Douglas Volcato <douglasvolcato@gmail.com>
2022-09-07 00:19:37 -0300

    Added mocks

---
+++ src/mocks/list.js
@@ -0,0 +1,69 @@
+export const list = [
+  {
+    id: 1,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 2,
+    note: "Assistir aula",
+    priority: "Alta",
+    time: "3 horas",
+  },
+  {
+    id: 3,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  { id: 4, note: "Estudar", priority: "Média", time: "1 hora" },
+  {
+    id: 5,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 6,
+    note: "Assistir aula",
+    priority: "Alta",
+    time: "3 horas",
+  },
+  {
+    id: 7,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 8,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 9,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 10,
+    note: "Assistir aula",
+    priority: "Alta",
+    time: "3 horas",
+  },
+  {
+    id: 11,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+  {
+    id: 12,
+    note: "Estudar",
+    priority: "Média",
+    time: "1 hora",
+  },
+];
